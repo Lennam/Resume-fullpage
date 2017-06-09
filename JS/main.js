f55ecfc2fe9c5e1ccd65b67c170c5cdf409a0751
Lennam <392003815@qq.com>
2017-06-09 15:03:24 +0800

    first commit

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+        verticalCentered: true,
+        resize: true,
+        navigation: true,
+        slidesNavigation: true,
+        navigationTooltips: ['简介','我的技能','我的作品','联系方式'],
+        sectionsColor : ['#33cc99', '#33cc99', '#33cc99','#33cc99'],
+
+    });
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# 我的简历
+<a href="https://lennam.github.io/Resume-fullpage/" target="_blank">浏览</a>
